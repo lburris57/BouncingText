85390f11085d40b584d0763d5a01bcedd4fca938
Larry Burris <lburris@mac.com>
2022-03-15 16:00:11 -0400

    Initial Commit

---
+++ BouncingText/BouncingTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  BouncingTextApp.swift
+//  BouncingText
+//
+//  Created by Larry Burris on 3/15/22.
+//
+
+import SwiftUI
+
+@main
+struct BouncingTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
